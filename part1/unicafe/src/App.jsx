ec75e83eab285177735921d981924ff0b5170f32
MM <michele.makiki@gmail.com>
2023-12-05 12:57:08 +0200

    step4

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -19,6 +19,15 @@ const positivePctCalculator = (good, all) => {
 }
 
 const Statistics = (props) => {
+  if (props.all === 0) {
+    return (
+      <div>
+        <h1>statistics</h1>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
+
   return (
     <div>
       <h1>statistics</h1>
